1bfb97d7312386618a4594e1b7135034c28bce2d
mubashir malik <malikmubashir601@gmail.com>
2022-01-05 01:41:25 +0500

    changes

--- src/axios.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = 'http://test1.foilsurfcamps.com/api/';
-axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
